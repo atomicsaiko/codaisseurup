50a4511c4f0a991e542d8b5bc69d94a1b81f8878
atomicsaiko <atomicsaiko@gmail.com>
2017-09-07 14:18:02 +0200

    Create Photo:Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,10 @@
 start_at = DateTime.strptime('2001-02-03T10:05:06+07:00', '%Y-%m-%dT%H:%M:%S%z')
 end_at = DateTime.strptime('2001-02-03T17:05:06+07:00', '%Y-%m-%dT%H:%M:%S%z')
 
+Photo.destroy_all
 Category.destroy_all
+Event.destroy_all
+Profile.destroy_all
 User.destroy_all
 
 kevin = User.create!(email: "kevin.chung@codaisseurup.com", password: "12345678")
@@ -22,6 +25,15 @@ racing = Category.create!(name: "Racing")
 food = Category.create!(name: "Food")
 sport = Category.create!(name: "Sport")
 
-Event.create!(name: "Paintball Sunday", description: "Bring your guns and mostly importantly, your enthusiasm!", location: "Amsterdam", price: 20, capacity: 25, includes_food: true, includes_drinks: true, starts_at: start_at, ends_at: end_at, active: true, user: kevin, categories: [nature, sport])
+event1 = Event.create!(name: "Paintball Sunday", description: "Bring your guns and mostly importantly, your enthusiasm!", location: "Amsterdam", price: 20, capacity: 25, includes_food: true, includes_drinks: true, starts_at: start_at, ends_at: end_at, active: true, user: kevin, categories: [nature, sport])
 
-Event.create!(name: "Amsterdam Pizza Festival", description: "Bring your Italian flavours!", location: "Amsterdam", price: 10, capacity: 35, includes_food: true, includes_drinks: true, starts_at: start_at, ends_at: end_at, active: true, user: jane, categories: [food, leisure])
+event2 = Event.create!(name: "Amsterdam Pizza Festival", description: "Bring your Italian flavours!", location: "Amsterdam", price: 10, capacity: 35, includes_food: true, includes_drinks: true, starts_at: start_at, ends_at: end_at, active: true, user: jane, categories: [food, leisure])
+
+# Photos
+photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event1)
+photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event1)
+photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event1)
+
+photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event2)
+photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event2)
+photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dgtx8b1vd/image/upload/v1504783695/pexels-photo-355988_siljb0.jpg", event: event2)
